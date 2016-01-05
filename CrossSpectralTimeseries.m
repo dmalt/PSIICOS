1f0c44e9e48c015e897f74caa1d324628d0eb7b0
Dmitriy Altukhov <dmalt@me.com>
2016-01-05 14:31:10 +0300

    new file:   CrossSpectralTimeseries.m

---
+++ CrossSpectralTimeseries.m
@@ -0,0 +1,86 @@
+function [ A, key,Coh ] = CrossSpectralTimeseries(X, bInducedOnly)
+
+if(nargin == 1)
+    bInducedOnly = false;
+end;
+
+[Nch, Ns  Ntr] = size(X);
+ERP = mean(X,3);
+
+Xfft = fft(X,[],2);
+h  = zeros(1,Ns); % nx1 for nonempty. 0x0 for empty.
+if Ns > 0 && 2 * fix(Ns / 2) == Ns
+  % even and nonempty
+  h([1 Ns/2+1]) = 1;
+  h(2:Ns/2) = 2;
+elseif Ns>0
+  % odd and nonempty
+  h(1) = 1;
+  h(2:(Ns+1)/2) = 2;
+end
+HF = repmat(h, [Nch, 1, Ntr]);
+XH = ifft(Xfft .* HF, [], 2);
+Xph = XH; %./(abs(XH)+0.0001*mean(abs(XH(:))));
+clear XH;
+clear Xfft;
+A = zeros(Nch * (Nch - 1) / 2, Ns);
+Nch_2 = Nch / 2;
+
+XphConj = conj(Xph);
+Ntr = size(Xph, 3);
+range = 1:Nch - 1;
+trs = 1:Ntr;
+k = 1;
+KEY = reshape(1:Nch * Nch, Nch, Nch);
+% we will take the diagonal as well
+A = zeros(Nch * (Nch + 1) / 2, Ns);
+fprintf('Calculating vectorised form of the cross spectral matrix upper triangle ... \n');
+fprintf('Reference sensor (max %d): ',Nch); 
+Coh = 1;
+
+if( 2 * fix(Ntr / 2) == Ntr)
+    trs_odd = 1:2:Ntr;
+    trs_even = 2:2:Ntr;
+else
+    trs_odd = 1:2:Ntr;
+    trs_even = 2:2:Ntr-1;
+end
+if(bInducedOnly)
+    for i=1:Nch
+        mn = (mean( bsxfun(@times,(Xph(1:Nch,:,trs_even) - Xph(1:Nch,:,trs_odd) ),XphConj(i,:,trs_even)-XphConj(i,:,trs_odd)),3));
+        A(k:k+Nch-1,:) = mn/2;
+        key(k:k+Nch-1) = KEY(1:Nch,i);
+        k = k+Nch;
+        if i>1
+          for j=0:log10(i-1)
+              fprintf('\b'); % delete previous counter display
+          end
+         end
+         fprintf('%d', i);
+    end;
+else
+    for i=1:Nch
+        bsx_temp = bsxfun(@times,Xph, XphConj(i,:,:));
+        mn = (mean(bsx_temp,3));
+        A(k:k+Nch-1,:) = mn;
+        key(k:k+Nch-1) = KEY(1:Nch,i);
+        k = k+Nch;
+        if i>1
+          for j=0:log10(i-1)
+              fprintf('\b'); % delete previous counter display
+          end
+         end
+         fprintf('%d', i);
+    end;
+end
+fprintf('\n');
+end
+
+
+
+
+
+
+
+
+
