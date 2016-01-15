28027582a382278780cbb15e939c380e5b2ed973
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-01-15 19:43:56 +0300

    Added docstring to RestoreSensorDimension.m 	modified:   RestoreSensorDimension.m

--- RestoreSensorDimension.m
+++ RestoreSensorDimension.m
@@ -1,3 +1,21 @@
 function CT_restored = RestoreSensorDimension(CT, UP)
+% -------------------------------------------------------
+% RestoreSensorDimension: transform cross-spectrum CT
+% back to real sensors from artificial ones.
+% -------------------------------------------------------
+% FORMAT:
+%   CT_restored = RestoreSensorDimension(CT, UP) 
+% INPUTS:
+%   CT        - {N_sensors_reduced x N_sensors_reduced}
+%               cross-spectrum matrix on artificial sensors
+%   UP        - {N_sensors x N_sensors_reduced} matrix;
+%               its columns are first N_sensors_reduced
+%               left singular vectors of gain matrix.
+% OUTPUTS:
+%   CT_restored   - {N_sensors x N_sensors} cross-spectrum 
+%                   matrix on real sensors
+% ______________________________________________________
+% Dmitrii Altukhov, dm.altukhov@ya.ru
+
 	CT_reshaped = reshape(CT, sqrt(size(CT, 1)), sqrt(size(CT,1)));
     CT_restored = UP' * CT_reshaped * UP;
