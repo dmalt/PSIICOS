bb1fe72d6299f749e9d65cb322c0e2a0fb7bb3fd
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-01-29 13:45:39 +0300

    Generalized RestoreSensorDimension to work with cross-spectral timeseries instead of cross-spectra itself. 	modified:   RestoreSensorDimension.m

--- RestoreSensorDimension.m
+++ RestoreSensorDimension.m
@@ -6,16 +6,18 @@ function CT_restored = RestoreSensorDimension(CT, UP)
 % FORMAT:
 %   CT_restored = RestoreSensorDimension(CT, UP) 
 % INPUTS:
-%   CT        - {N_sensors_reduced x N_sensors_reduced}
+%   CT        - {N_sensors_reduced ^ 2 x Ntimes}
 %               cross-spectrum matrix on artificial sensors
 %   UP        - {N_sensors x N_sensors_reduced} matrix;
 %               its columns are first N_sensors_reduced
 %               left singular vectors of gain matrix.
 % OUTPUTS:
-%   CT_restored   - {N_sensors x N_sensors} cross-spectrum 
+%   CT_restored   - {N_sensors ^ 2 x Ntimes} cross-spectrum 
 %                   matrix on real sensors
 % ______________________________________________________
 % Dmitrii Altukhov, dm.altukhov@ya.ru
-
-	CT_reshaped = reshape(CT, sqrt(size(CT, 1)), sqrt(size(CT,1)));
-    CT_restored = UP' * CT_reshaped * UP;
+	for t = 1:size(CT,2)
+		CT_reshaped = reshape(CT(:,t), sqrt(size(CT, 1)), sqrt(size(CT,1)));
+	    CT_sq_restored = UP' * CT_reshaped * UP;
+	    CT_restored(:,t) = CT_sq_restored(:);
+	end
