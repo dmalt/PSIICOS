9ea8efebf6238af9af2399d37aa8883b4016355f
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-02-17 23:01:04 +0300

    new file:   UpperDiagToPairs.m

---
+++ UpperDiagToPairs.m
@@ -0,0 +1,23 @@
+function IND = UpperDiagToPairs(N)
+% -------------------------------------------------------
+% UpperDiagToPairs: for an {N x N} matrix make a mapping 
+% index array IND which enumerates matrix elements above
+% the diagonal. Pairs go rowwise, meaning that IND(1) is 
+% a [1,2] pair, IND(2) is [1,3] and so on.
+% Example:
+% [i,j] = IND(p),
+% where p is a number of a pair 
+% -------------------------------------------------------
+% FORMAT:
+%   IND = UpperDiagToPairs(Nsites) 
+% INPUTS:
+%   N             - 
+% OUTPUTS:
+%   IND           - {N * (N - 1) / 2 x 2} matrix;
+% ________________________________________
+% Dmitrii Altukhov, dm.altukhov@ya.ru
+	for i = 1:N
+		for j = i + 1: N
+			IND(p,:) = [i,j];
+		end
+	end
