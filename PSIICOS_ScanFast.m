74fe30c45a589c78b75fe31582840f2a4f9776ee
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-02-20 20:42:37 +0300

    Checked out a version of PSIICOS scan for a multidimensional signal space 	modified:   PSIICOS_ScanFast.m

--- PSIICOS_ScanFast.m
+++ PSIICOS_ScanFast.m
@@ -16,13 +16,14 @@ function [Cs, IND Cs0] = PSIICOS_ScanFast(G2dU, Cp)
     [Nsns, Nsrc2] = size(G2dU);
     Nsrc = Nsrc2 / 2;
 
-    if(size(Cp,1)~=Nsns)
-        disp('Incomptible dimensions G2dU vs Cp');
-        return;
-    end
+    % if(size(Cp,1)~=Nsns)
+    %     disp('Incomptible dimensions G2dU vs Cp');
+    %     return;
+    % end
 
-    T = zeros(1, Nsrc * (Nsrc - 1) / 2);
-    D = zeros(1, Nsrc * (Nsrc - 1) / 2);
+    n_comp = size(Cp, 2);
+    T = zeros(n_comp, Nsrc * (Nsrc - 1) / 2);
+    D = zeros(n_comp, Nsrc * (Nsrc - 1) / 2);
     Ti = zeros(1, Nsrc);
     Di = zeros(1, Nsrc);
     IND = zeros(Nsrc * (Nsrc - 1) / 2, 2);
@@ -37,6 +38,7 @@ function [Cs, IND Cs0] = PSIICOS_ScanFast(G2dU, Cp)
     cs2_11_22 = zeros(2, Nsrc);
     cs2_12_21 = zeros(1, Nsrc);
     Cs0 =  zeros(1, Nsrc * (Nsrc - 1) / 2);
+
     %below is the optimized implementation of this:
     % Look at each pair and estimate subspace correlation
     % between cross-spectrum and topography of this pair
@@ -59,26 +61,30 @@ function [Cs, IND Cs0] = PSIICOS_ScanFast(G2dU, Cp)
     tic
     p = 1;
     ai = zeros(2, Nsns);
-    for i=1:Nsrc
-        % --- Take i-th location topographies ---- %
-        ai = G2dU(:, i * 2 - 1 : i * 2)';       
-        tmp = ai * Cp;
-        cslong =  tmp * G2dU;
-        cs2long = cslong .* conj(cslong);
-        cs2longd = cslong(1,:) .* conj(cslong(2,:));
-        cs2_11_22 = [sum(reshape(cs2long(1,:), 2, Nsrc), 1);...
-                    sum(reshape(cs2long(2,:), 2, Nsrc), 1)];
-        cs2_12_21 = sum(reshape(cs2longd, 2, Nsrc), 1);
-        Ti = sum(cs2_11_22, 1);
-        Di = prod(cs2_11_22, 1) - cs2_12_21 .* conj(cs2_12_21);
-        T(p : p + Nsrc - 1 - i) = Ti(i + 1 : Nsrc);
-        D(p : p + Nsrc - 1 - i) = Di(i + 1 : Nsrc);
-        IND(p : p + Nsrc - i - 1, 2) = (i + 1 : Nsrc)';
-        IND(p : p + Nsrc - i - 1, 1) = i;
-        p = p + Nsrc - i;
+    for iSrc = 1:Nsrc
+        for iComp = 1:n_comp
+            Cp_sq = reshape(Cp(:,iComp), Nsns, Nsns);
+            % --- Take iSrc-th location topographies ---- %
+            ai = G2dU(:, iSrc * 2 - 1 : iSrc * 2)';       
+            tmp = ai * Cp_sq;
+            cslong =  tmp * G2dU;
+            cs2long = cslong .* conj(cslong);
+            cs2longd = cslong(1,:) .* conj(cslong(2,:));
+            cs2_11_22 = [sum(reshape(cs2long(1,:), 2, Nsrc), 1);...
+                        sum(reshape(cs2long(2,:), 2, Nsrc), 1)];
+            cs2_12_21 = sum(reshape(cs2longd, 2, Nsrc), 1);
+            Ti = sum(cs2_11_22, 1);
+            Di = prod(cs2_11_22, 1) - cs2_12_21 .* conj(cs2_12_21);
+            T(iComp, p : p + Nsrc - 1 - iSrc) = Ti(iSrc + 1 : Nsrc);
+            D(iComp, p : p + Nsrc - 1 - iSrc) = Di(iSrc + 1 : Nsrc);
+        end
+        IND(p : p + Nsrc - iSrc - 1, 2) = (iSrc + 1 : Nsrc)';
+        IND(p : p + Nsrc - iSrc - 1, 1) = iSrc;
+        p = p + Nsrc - iSrc;
     end;
     Cs = 0.5 * T + sqrt(0.25 * T .* (T) - D); 
-           
+    % Cs = sum(Cs,1);  
+    Cs = max(Cs,[],1);    
     toc
 
 
