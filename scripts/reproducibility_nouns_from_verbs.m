d797e62c3ceb649cd928fb821fb2ad5c41227091
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-07-07 16:06:53 -0400

    Reproducibility_nouns_from_verbs: make a graph of reproducibility vs rank of projection from condition 4 for full CT

--- scripts/reproducibility_nouns_from_verbs.m
+++ scripts/reproducibility_nouns_from_verbs.m
@@ -1,13 +1,6 @@
-subjNames = {   '0003_pran', ... % 1
-				'0019_shev', ... % 2
-				'0030_koal', ... % 3
-				'0108_bami', ... % 3
-				'0062_peek', ... % 5
-				'0074_kuni', ... % 6
-				'0100_kase', ... % 7
-				'0106_supo', ... % 8
-				'0109_zvma', ... % 9
-				'0130_hagr'};    % 10
+clear all;
+setup_subjNames
+setup_chLoc
 
 cond_noun = '2';
 cond_verb = '4';
@@ -17,34 +10,39 @@ sFreq = 500;
 
 
 % --------------- 3. PSIICOS .3 ------------------ % 
-h_fig = figure;
-set(h_fig, 'name', 'PSIICOS', 'numbertitle', 'off');
 for iSubj = 1:length(subjNames)
 % for iSubj = 1:1
 	curName = subjNames{iSubj};
 	HM = LoadHeadModel(curName, cond_noun);
-	trials_noun = LoadTrials(curName, cond_noun, band, tRange);
-	trials_verb = LoadTrials(curName, cond_verb, band, tRange);
+	trials_2 = LoadTrials(curName, cond_noun, band, tRange);
+	trials_4 = LoadTrials(curName, cond_verb, band, tRange);
 
-	CT_noun = CrossSpectralTimeseries(trials_noun.data);
-	CT_verb = CrossSpectralTimeseries(trials_verb.data);
-	CT = ProjAwayFromCond(CT_noun, CT_verb);
-
-	CT = ProjectAwayFromPowerComplete(CT, HM.gain);
-	CT = RestoreCTdim(CT, HM.UP);
-	conInds{iSubj} = SensorConnectivity((CT), 100);
-	subplot(2,5,iSubj);
-	h_ax = DrawConnectionsOnSensors(conInds{iSubj});
-	% h_title = title(h_ax, [num2str(band), ' Hz ', num2str(tRange), ' s'] );
-	view(h_ax, [0,90]);
+	CT_2 = CrossSpectralTimeseries(trials_2.data);
+	CT_4 = CrossSpectralTimeseries(trials_4.data);
+	iRnk = 1;
+	for rnk = 0:20
+		CT_2_from_4 = ProjFromCond(CT_2, CT_4, rnk);
+		CT_2_from_4 = ProjectAwayFromPowerComplete(CT_2_from_4, HM.gain);
+		CT_2_from_4 = RestoreCTdim(CT_2_from_4, HM.UP);
+		conInds_2_from_4{iSubj}{iRnk} = SensorConnectivity((CT_2_from_4), 100);
+		iRnk = iRnk + 1;
+	end
 end
 
-CSa = zeros(10,10);
-for s1 = 1:10
-   for s2 = 1:10
-       if(s1~=s2)
-           CSa(s1,s2) = ConnectivitySimilarity(conInds{s1}, conInds{s2}, ChLoc);
-       end
-   end;
-end;
-% ------------------------------------------------ %
+iRnk = 1;
+for rnk = 0:20
+	count = 1;
+	for s1 = 1:10
+	   for s2 = s1+1:10
+	       if(s1~=s2)
+	           CS_2_from_4(count, iRnk) = ConnectivitySimilarity(conInds_2_from_4{s1}{iRnk}, conInds_2_from_4{s2}{iRnk}, ChLoc);
+	           count = count + 1;
+	       end
+	   end;
+	end;
+
+	mean_2_from_4(iRnk) = mean(CS_2_from_4(:, iRnk));
+	std_2_from_4(iRnk) = std(CS_2_from_4(:,iRnk));
+	iRnk = iRnk + 1;
+end
+	% ------------------------------------------------ %
