a8639460997e8bfd1a2739582e98c2203f52d367
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-07-08 16:15:00 -0400

    With difference between conditions

--- scripts/reproducibility_nouns_from_verbs.m
+++ scripts/reproducibility_nouns_from_verbs.m
@@ -3,7 +3,7 @@ setup_subjNames
 setup_chLoc
 
 cond_noun = '2';
-cond_pseudoword = '1';
+cond_pseudoword = '4';
 band = [16,25];
 tRange = [0,0.7];
 sFreq = 500;
@@ -23,29 +23,24 @@ for iSubj = 1:length(subjNames)
 	CT_4 = CrossSpectralTimeseries(trials_4.data);
 	CT_4 = ProjectAwayFromPowerComplete(CT_4, HM.gain);
 	iRnk = 1;
-	for rnk = 0:20
-		CT_2_from_4 = ProjFromCond(CT_2, CT_4, rnk);
-		CT_2_from_4 = RestoreCTdim(CT_2_from_4, HM.UP);
-		conInds_2_from_4{iSubj}{iRnk} = SensorConnectivity((CT_2_from_4), 100);
-		iRnk = iRnk + 1;
-	end
+	
+	CT_diff = CT_2 - CT_4;
+	CT_diff = RestoreCTdim(CT_diff, HM.UP);
+	conInds_diff{iSubj} = SensorConnectivity((CT_diff), 100);
 end
 
-iRnk = 1;
-for rnk = 0:20
-	count = 1;
-	for s1 = 1:10
-	   for s2 = s1+1:10
-	       if(s1~=s2)
-	           CS_2_from_4(count, iRnk) = ConnectivitySimilarity(conInds_2_from_4{s1}{iRnk}, conInds_2_from_4{s2}{iRnk}, ChLoc);
-	           count = count + 1;
-	       end
-	   end;
-	end;
 
-	mean_2_from_4(iRnk) = mean(CS_2_from_4(:, iRnk));
-	std_2_from_4(iRnk) = std(CS_2_from_4(:,iRnk));
-	iRnk = iRnk + 1;
-end
-errorbar(0:20, mean_2_from_4, std_2_from_4 ./ sqrt(10), 'bs-')
+count = 1;
+for s1 = 1:10
+   for s2 = s1+1:10
+       if(s1~=s2)
+           CS_diff(count) = ConnectivitySimilarity(conInds_diff{s1}, conInds_diff{s2}, ChLoc);
+           count = count + 1;
+       end
+   end;
+end;
+
+
+mean_diff = mean(CS_diff)
+
 	% ------------------------------------------------ %
