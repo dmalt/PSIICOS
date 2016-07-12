423508e56d84b1d9404ef9ba67a6a6d313c17f97
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-07-12 11:26:08 -0400

    Added align_trials

--- scripts/reproducibility_nouns_from_verbs.m
+++ scripts/reproducibility_nouns_from_verbs.m
@@ -2,10 +2,10 @@ clear all;
 setup_subjNames
 setup_chLoc
 
-cond_main = '2';
-cond_proj_from = '4';
-band = [18,22];
-tRange = [0,0.3];
+cond_noun = '2';
+cond_pseudoword = '1';
+band = [16,25];
+tRange = [0,0.7];
 sFreq = 500;
 
 
@@ -13,39 +13,39 @@ sFreq = 500;
 for iSubj = 1:length(subjNames)
 % for iSubj = 1:1
 	curName = subjNames{iSubj};
-	HM = LoadHeadModel(curName, cond_main);
-	trials_main = LoadTrials(curName, cond_main, band, tRange);
-	trials_proj_from = LoadTrials(curName, cond_proj_from, band, tRange);
+	HM = LoadHeadModel(curName, cond_noun);
+	trials_2 = LoadTrials(curName, cond_noun, band, tRange);
+	trials_4 = LoadTrials(curName, cond_pseudoword, band, tRange);
 
-	CT_m = CrossSpectralTimeseries(trials_main.data);
-	CT_m = ProjectAwayFromPowerComplete(CT_m, HM.gain);
-	CT_m = RestoreCTdim(CT_m, HM.UP);
+	CT_2 = CrossSpectralTimeseries(trials_2.data);
+	CT_2 = ProjectAwayFromPowerComplete(CT_2, HM.gain);
 
-	% CT_p = CrossSpectralTimeseries(trials_proj_from.data);
-	% CT_p = ProjectAwayFromPowerComplete(CT_p, HM.gain);
-	
-	% CT_diff = CT_m - CT_p;
-	conInds_full{iSubj} = SensorConnectivity((CT_m), 100);
-	conInds_real{iSubj} = SensorConnectivity(real(CT_m), 100);
-	conInds_imag{iSubj} = SensorConnectivity(imag(CT_m), 100);
+	CT_4 = CrossSpectralTimeseries(trials_4.data);
+	CT_4 = ProjectAwayFromPowerComplete(CT_4, HM.gain);
+	iRnk = 1;
+	for rnk = 0:20
+		CT_2_from_4 = ProjFromCond(CT_2, CT_4, rnk);
+		CT_2_from_4 = RestoreCTdim(CT_2_from_4, HM.UP);
+		conInds_2_from_4{iSubj}{iRnk} = SensorConnectivity((CT_2_from_4), 100);
+		iRnk = iRnk + 1;
+	end
 end
 
+iRnk = 1;
+for rnk = 0:20
+	count = 1;
+	for s1 = 1:10
+	   for s2 = s1+1:10
+	       if(s1~=s2)
+	           CS_2_from_4(count, iRnk) = ConnectivitySimilarity(conInds_2_from_4{s1}{iRnk}, conInds_2_from_4{s2}{iRnk}, ChLoc);
+	           count = count + 1;
+	       end
+	   end;
+	end;
 
-count = 1;
-for s1 = 1:10
-   for s2 = s1+1:10
-       if(s1~=s2)
-           CS_full(count) = ConnectivitySimilarity(conInds_full{s1}, conInds_full{s2}, ChLoc);
-           CS_real(count) = ConnectivitySimilarity(conInds_real{s1}, conInds_real{s2}, ChLoc);
-           CS_imag(count) = ConnectivitySimilarity(conInds_imag{s1}, conInds_imag{s2}, ChLoc);
-           count = count + 1;
-       end
-   end;
-end;
-
-
-mean_full = mean(CS_full)
-mean_real = mean(CS_real)
-mean_imag = mean(CS_imag)
-
+	mean_2_from_4(iRnk) = mean(CS_2_from_4(:, iRnk));
+	std_2_from_4(iRnk) = std(CS_2_from_4(:,iRnk));
+	iRnk = iRnk + 1;
+end
+errorbar(0:20, mean_2_from_4, std_2_from_4 ./ sqrt(10), 'bs-')
 	% ------------------------------------------------ %
