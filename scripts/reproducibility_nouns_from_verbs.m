ec4b52abe1ead9cd272efa0be2586b26dc79cea0
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-07-13 14:51:19 -0400

    Proj 4 from 1

--- scripts/reproducibility_nouns_from_verbs.m
+++ scripts/reproducibility_nouns_from_verbs.m
@@ -2,81 +2,36 @@ clear all;
 setup_subjNames
 setup_chLoc
 
-cond_main = '2';
-cond_proj_from = '4';
+cond_noun = '4';
+cond_pseudoword = '1';
 band = [18,22];
 tRange = [0,0.7];
 sFreq = 500;
 
-d_thresh = 0.001;
 
 % --------------- 3. PSIICOS .3 ------------------ % 
 for iSubj = 1:length(subjNames)
 % for iSubj = 1:1
 	curName = subjNames{iSubj};
-	HM = LoadHeadModel(curName, cond_main);
-	trials_main = LoadTrials(curName, cond_main, band, tRange);
-	% trials_proj_from = LoadTrials(curName, cond_proj_from, band, tRange);
+	HM = LoadHeadModel(curName, cond_noun);
+	trials_2 = LoadTrials(curName, cond_noun, band, tRange);
+	trials_4 = LoadTrials(curName, cond_pseudoword, band, tRange);
 
-	CT_m = CrossSpectralTimeseries(trials_main.data);
-	CT_m = ProjectAwayFromPowerComplete(CT_m, HM.gain);
-	CT_m = RestoreCTdim(CT_m, HM.UP);
+	CT_2 = CrossSpectralTimeseries(trials_2.data);
+	CT_2 = ProjectAwayFromPowerComplete(CT_2, HM.gain);
 
-	% CT_p = CrossSpectralTimeseries(trials_proj_from.data);
-	% CT_p = ProjectAwayFromPowerComplete(CT_p, HM.gain);
-	
-	% CT_diff = CT_m - CT_p;
-	conInds_full{iSubj} = SensorConnectivity((CT_m), 200);
-	conInds_real{iSubj} = SensorConnectivity(real(CT_m), 200);
-	conInds_imag{iSubj} = SensorConnectivity(imag(CT_m), 200);
+	CT_4 = CrossSpectralTimeseries(trials_4.data);
+	CT_4 = ProjectAwayFromPowerComplete(CT_4, HM.gain);
 
-	for iCon = 1:length(conInds_full{iSubj})
-		xyz1 = ChLoc(:, conInds_full{iSubj}(iCon,1));
-		xyz2 = ChLoc(:, conInds_full{iSubj}(iCon,2));
-		d12 = norm(xyz1 - xyz2);
-		if d12 < d_thresh
-			conInds_full{iSubj}(iCon,:) = [-1, -1];
-		end
-	end
+	CT_2_from_4 = ProjFromCond(CT_2, CT_4, 6);
+	CT_2_from_4 = RestoreCTdim(CT_2_from_4, HM.UP);
+	% conInds_2_from_4{iSubj} = SensorConnectivity((CT_2_from_4), 100);
 
-	conInds_full{iSubj}(conInds_full{iSubj}(:,1) == -1, :) = [];
-	conInds_full{iSubj} = conInds_full{iSubj}(1:100,:);
+	conInds_full{iSubj} = SensorConnectivity((CT_2_from_4), 100);
+    conInds_real{iSubj} = SensorConnectivity(real(CT_2_from_4), 100);
+    conInds_imag{iSubj} = SensorConnectivity(imag(CT_2_from_4), 100);
 
-	for iCon = 1:length(conInds_real{iSubj})
-		xyz1 = ChLoc(:, conInds_real{iSubj}(iCon,1));
-		xyz2 = ChLoc(:, conInds_real{iSubj}(iCon,2));
-		d12 = norm(xyz1 - xyz2);
-		if d12 < d_thresh
-			conInds_real{iSubj}(iCon,:) = [-1, -1];
-		end
-	end
-
-	conInds_real{iSubj}(conInds_real{iSubj}(:,1) == -1, :) = [];
-	conInds_real{iSubj} = conInds_real{iSubj}(1:100,:);
-
-
-	for iCon = 1:length(conInds_imag{iSubj})
-		xyz1 = ChLoc(:, conInds_imag{iSubj}(iCon,1));
-		xyz2 = ChLoc(:, conInds_imag{iSubj}(iCon,2));
-		d12 = norm(xyz1 - xyz2);
-		if d12 < d_thresh
-			conInds_imag{iSubj}(iCon,:) = [-1, -1];
-		end
-	end
-
-	conInds_imag{iSubj}(conInds_imag{iSubj}(:,1) == -1, :) = [];
-	conInds_imag{iSubj} = conInds_imag{iSubj}(1:100,:);
-
-	% nCon = min([length(conInds_full{iSubj}),...
-	% 		    length(conInds_full{iSubj}),...
-	% 		    length(conInds_full{iSubj})])
-	% conInds_full{iS} = 
 end
-
-
-
-
-
 
 count = 1;
 for s1 = 1:10
@@ -94,5 +49,4 @@ end;
 mean_full = mean(CS_full)
 mean_real = mean(CS_real)
 mean_imag = mean(CS_imag)
-
 	% ------------------------------------------------ %
