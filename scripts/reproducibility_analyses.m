5cce829e73b719e6524c18da99afe36d0845d370
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-07-14 20:35:02 -0400

    Renamed and improved reproducibility_nouns_from_verbs.m

--- scripts/reproducibility_analyses.m
+++ scripts/reproducibility_analyses.m
@@ -11,28 +11,29 @@ d_min = 0.05; % minimal allowed length for connections
 nConn_ini = 200; % initial number of connections
 nConn = 100; % number of connections to end up with
 
-% --------------- 3. PSIICOS .3 ------------------ % 
+% -------------------------- <><><><><><><><><><> ----------------------------- % 
 for iSubj = 1:length(subjNames)
 	curName = subjNames{iSubj};
 
 	HM = LoadHeadModel(curName, main_cond);
 
 	tr_m   = LoadTrials(curName, main_cond, band, tRange);
-	tr_prf = LoadTrials(curName, proj_cond, band, tRange);
+	% tr_prf = LoadTrials(curName, proj_cond, band, tRange);
 
 	CT_m   = CrossSpectralTimeseries(tr_m.data);
-	CT_prf = CrossSpectralTimeseries(tr_prf.data);
+	% CT_prf = CrossSpectralTimeseries(tr_prf.data);
 
 	CT_m   = ProjectAwayFromPowerComplete(CT_m, HM.gain);
-	CT_prf = ProjectAwayFromPowerComplete(CT_prf, HM.gain);
+	% CT_prf = ProjectAwayFromPowerComplete(CT_prf, HM.gain);
 
-	CT_pr = ProjFromCond(CT_m, CT_prf, 6);
+	% CT_final = ProjFromCond(CT_m, CT_prf, 6);
+	CT_final = CT_m; 
 
-	CT_pr = RestoreCTdim(CT_pr, HM.UP);
+	CT_final = RestoreCTdim(CT_final, HM.UP);
 
-	conInds_full{iSubj} = GetSensorConnectivity(CT_pr,       nConn_ini);
-	conInds_real{iSubj} = GetSensorConnectivity(real(CT_pr), nConn_ini);
-	conInds_imag{iSubj} = GetSensorConnectivity(imag(CT_pr), nConn_ini);
+	conInds_full{iSubj} = GetSensorConnectivity(CT_final,       nConn_ini);
+	conInds_real{iSubj} = GetSensorConnectivity(real(CT_final), nConn_ini);
+	conInds_imag{iSubj} = GetSensorConnectivity(imag(CT_final), nConn_ini);
 
 	conInds_full{iSubj} = DropLongConn(conInds_full{iSubj}, ChLoc, d_min, nConn);
 	conInds_real{iSubj} = DropLongConn(conInds_real{iSubj}, ChLoc, d_min, nConn);
@@ -42,4 +43,4 @@ end
 [mean_full, std_full, CS_full] = ConnSimMetrics(conInds_full, ChLoc);
 [mean_real, std_real, CS_real] = ConnSimMetrics(conInds_real, ChLoc);
 [mean_imag, std_imag, CS_imag] = ConnSimMetrics(conInds_imag, ChLoc);
-	% ------------------------------------------------ %
+% ------------------------- <><><><><><><><><><><> ---------------------------- %
