a80feffebc1e540d0aa9988d4e081e139b567a44
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-07-18 11:25:46 -0400

    Added p-vals

--- scripts/reproducibility_analyses.m
+++ scripts/reproducibility_analyses.m
@@ -1,11 +1,13 @@
-% clear all;
+clear conInds_full;
+clear conInds_real;
+clear conInds_imag;
 setup_subjNames
 setup_chLoc
 
 main_cond = '2';
 proj_cond = '1';
 band = [18,22];
-tRange = [0,0.7];
+tRange = [0.2,0.3];
 sFreq = 500;
 d_min = 0.05; % minimal allowed length for connections
 nConn_ini = 200; % initial number of connections
@@ -18,16 +20,16 @@ for iSubj = 1:length(subjNames)
 	HM = LoadHeadModel(curName, main_cond);
 
 	tr_m   = LoadTrials(curName, main_cond, band, tRange);
-	% tr_prf = LoadTrials(curName, proj_cond, band, tRange);
+	tr_prf = LoadTrials(curName, proj_cond, band, tRange);
 
 	CT_m   = CrossSpectralTimeseries(tr_m.data);
-	% CT_prf = CrossSpectralTimeseries(tr_prf.data);
+	CT_prf = CrossSpectralTimeseries(tr_prf.data);
 
 	CT_m   = ProjectAwayFromPowerComplete(CT_m, HM.gain);
-	% CT_prf = ProjectAwayFromPowerComplete(CT_prf, HM.gain);
+	CT_prf = ProjectAwayFromPowerComplete(CT_prf, HM.gain);
 
-	% CT_final = ProjFromCond(CT_m, CT_prf, 6);
-	CT_final = CT_m; 
+	CT_final = ProjFromCond(CT_m, CT_prf, 6);
+	% CT_final = CT_m; 
 
 	CT_final = RestoreCTdim(CT_final, HM.UP);
 
@@ -43,4 +45,12 @@ end
 [mean_full, std_full, CS_full] = ConnSimMetrics(conInds_full, ChLoc);
 [mean_real, std_real, CS_real] = ConnSimMetrics(conInds_real, ChLoc);
 [mean_imag, std_imag, CS_imag] = ConnSimMetrics(conInds_imag, ChLoc);
+
+mean_full
+mean_real
+mean_imag
+
+p_full_real = ranksum(CS_full, CS_real)
+p_full_imag = ranksum(CS_full, CS_imag)
+p_imag_real = ranksum(CS_imag, CS_real)
 % ------------------------- <><><><><><><><><><><> ---------------------------- %
