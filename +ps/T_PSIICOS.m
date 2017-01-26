33bf3ee7478c3bfe49f0904e7a1c34bb498ad256
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-01-26 17:14:26 +0300

    Added threshold_connections call

--- +ps/T_PSIICOS.m
+++ +ps/T_PSIICOS.m
@@ -30,6 +30,7 @@ function [INDrap, Cpvec, Upwr, corr] = T_PSIICOS(C, G2dU, rel_threshold, Rnk, Si
 
     import ps.ProjectAwayFromPowerComplete
     import ps.PSIICOS_ScanFast
+    import ups.threshold_connections
 
     %% Preparatory steps
     % if(nargin < 5)
@@ -53,7 +54,7 @@ function [INDrap, Cpvec, Upwr, corr] = T_PSIICOS(C, G2dU, rel_threshold, Rnk, Si
     end;
 
     Nsrc = size(G2dU, 2) / 2; % two topography columns per each source of the grid
-    Nch = size(G2dU, 1);
+    % Nch = size(G2dU, 1);
     % perform projection of the coherence matrix away from the power only
     if(isempty(Upwr))
         % [Cpvec, Upwr] = ProjectAwayFromPowerFixedOr(C(:), G2dU, Rnk);
@@ -65,13 +66,13 @@ function [INDrap, Cpvec, Upwr, corr] = T_PSIICOS(C, G2dU, rel_threshold, Rnk, Si
     % Cpvec = C;
     
     if SigRnk
-        [uc,sc,vc] = svd(Cpvec, 'econ');
+        [uc,~,~] = svd(Cpvec, 'econ');
         Cp = uc(:,1:SigRnk);
     elseif SigRnk == 0
         Cp = sum(Cpvec,2);
         Cp = Cp / norm(Cp, 'fro');
     else
-        fprintf('ERROR: T_PSIICOS: Signal space rank %f\n is not valid', SigRnk );
+        fprintf('ERROR: T_PSIICOS: Signal space rank %f\n is not valid', SigRnk);
         return;
     end
     %% normalize forward matrix
@@ -83,28 +84,30 @@ function [INDrap, Cpvec, Upwr, corr] = T_PSIICOS(C, G2dU, rel_threshold, Rnk, Si
      end;
 
     %% scan all pairs with efficient vectorised implementation
-    Urap = [];
-    range2 = 1:2;
-    indep_topo = zeros(prod(size(Cp)), 2);
-    c_ss_hat = zeros(1, 2);
+    % Urap = [];
+    % range2 = 1:2;
+    % indep_topo = zeros(prod(size(Cp)), 2);
+    % c_ss_hat = zeros(1, 2);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     % Look at the topography of a pair that is
     % most correlated with the cross-spectrum
+    size(Cp)
     [corr(1,:), IND] = PSIICOS_ScanFast(G2dU, Cp);
-    corr_min  = min(corr);
-    corr_max = max(corr);
-    corr_delta = corr_max - corr_min;
-    corr_threshold = corr_min + corr_delta * rel_threshold;
-    % ind_threshold = find(sqrt(corr * 2) > corr_threshold);
-    ind_threshold = find(corr > corr_threshold);
-    pair_max = IND(ind_threshold,:);
-    i = IND(ind_threshold, 1); 
-    j = IND(ind_threshold, 2);
-    INDrap(:,1) = i;
-    INDrap(:,2) = j;
+
+    % corr_min  = min(corr);
+    % corr_max = max(corr);
+    % corr_delta = corr_max - corr_min;
+    % corr_threshold = corr_min + corr_delta * rel_threshold;
+    % % ind_threshold = find(sqrt(corr * 2) > corr_threshold);
+    % ind_threshold = find(corr > corr_threshold);
+    % pair_max = IND(ind_threshold,:);
+    % i = IND(ind_threshold, 1); 
+    % j = IND(ind_threshold, 2);
+    INDrap = threshold_connections(corr, rel_threshold, IND);
+    % INDrap(:,1) = i;
+    % INDrap(:,2) = j;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
+end
