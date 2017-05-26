24b7542719bcfa8368c76b55898bbf8420d914c0
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-05-27 01:18:56 +0300

    retab

--- +ps/ProjFromCond.m
+++ +ps/ProjFromCond.m
@@ -16,14 +16,14 @@ function CT = ProjFromCond(CT1, CT2, rnk)
 %                condition 2
 % ________________________________________
 % Dmitrii Altukhov, dm.altukhov@ya.ru
-	if nargin < 3
-		rnk = 6;
-	end
+    if nargin < 3
+        rnk = 6;
+    end
 
-	[u, ~, ~] = svd(CT2);
-	if rnk <= size(u, 2)
-		CT = CT1 - u(:,1:rnk) * u(:,1:rnk)' * CT1;
-	else
-		error('InputError: ValueOutOfRange');
-	end
+    [u, ~, ~] = svd(CT2);
+    if rnk <= size(u, 2)
+        CT = CT1 - u(:,1:rnk) * u(:,1:rnk)' * CT1;
+    else
+        error('InputError: ValueOutOfRange');
+    end
 end
