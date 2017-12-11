bfa6f315f1d46610ef35e248f8d2224e52c3514c
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-12-11 18:50:43 +0300

    checkout reproduce_4_comps from dev

--- scripts/biomag2010/reproduce_4_comps.m
+++ scripts/biomag2010/reproduce_4_comps.m
@@ -1,50 +1,159 @@
+% Bootstrap cross-spetcrum timeseries, then compute SVD
+% on real and imaginary parts on each resample and do
+% FastScan for first 4 components in real and imag parts
+% AUTHOR: dmalt
+% DATE Sun Nov 19 22:14:01 MSK 2017
+% ______________________________________________________
+
 main
 
 time_range = [0, 1];
-
-alpha_band = [8,12];
-beta_band = [16,24];
-
-freq_band = beta_band;
-% freq_band = alpha_band;
+% time_range = [-0.5, 1];
 
 ltr     = memoize(@load_trials);
 calc_CT = memoize(@ups.conn.CrossSpectralTimeseries);
 proj    = memoize(@ps.ProjectAwayFromPowerComplete);
 msvd    = memoize(@svd);
 
+Upwr = ps.GetUpwrComplete(HM.gain, pwr_rnk);
+% Upwr = ps.GetUpwrComplete(HM.gain, 101);
+
+for i_band = 7:length(bands)
+% freq_band = beta_band;
+freq_band = bands{i_band};
+% freq_band = alpha_band;
+
 tr_filt= ltr(data_path, time_range, freq_band, HM);
-CT = calc_CT(tr_filt, true);
-
-
-CT_proj = proj(CT, HM.gain, pwr_rnk);
-
-[u,s,v] = msvd(imag(CT_proj));
-% [u,s,v] = msvd(imag(CT_proj));
-
+% CT = calc_CT(tr_filt, true);
+% CT = ups.GetFakeCT(size(HM.gain,1), 1200);
+CTs = ups.bootstrap_CT(tr_filt, 100, true);
+% profile on;
 for i_comp = 1:4
-    [CS, IND] = ps.PSIICOS_ScanFast(HM.gain, u(:,i_comp));
-    con_inds{i_comp} = ups.threshold_connections(CS, threshold, IND);
+% i_comp = 1;
+    for i_resamp = 1:length(CTs)
+        tic
+        CT_proj = CTs{i_resamp} - Upwr * (Upwr' * CTs{i_resamp});
+        % CT_proj = CT - Upwr * (Upwr' * CT);
+        [u_re,~,v_re] = msvd(real(CT_proj));
+        [u_im,~,v_im] = msvd(imag(CT_proj));
+        % [u,s,v] = msvd(imag(CT_proj));
+        % [u,s,v] = msvd(imag(CT));
+        vs_re{i_comp}{i_resamp} = v_re(:,i_comp);
+        vs_im{i_comp}{i_resamp} = v_im(:,i_comp);
+        [CS_re, ~] = ps.PSIICOS_ScanFast(HM.gain, u_re(:,i_comp));
+        [CS_im, IND] = ps.PSIICOS_ScanFast(HM.gain, u_im(:,i_comp));
+        % con_inds_re{i_comp}{i_resamp} = ups.threshold_connections(CS_re, threshold, IND);
+        con_inds_re{i_comp}{i_resamp} = ups.threshold_connections(CS_re, threshold, IND);
+        con_inds_im{i_comp}{i_resamp} = ups.threshold_connections(CS_im, threshold, IND);
+        toc
+    end
+end
+save([band_names{i_band}, '.mat'], 'vs_re', 'vs_im', 'con_inds_re', 'con_inds_im', 'CTs', '-v7.3');
 end
 
-con = ups.Connections(con_inds, HM, CtxHHR);
-% con.CT = CT_proj;
-con.CT = CT_proj;
-con_av = con.Average();
+return;
+%  Things to check bootstrap solution {{{1 %
+    con = ups.Bundles(con_inds_re{1}, HM, CtxInfl);
+    con.Plot()
+    con_av = con.Average();
+    i = con_av.conInds{1}(1);
+    j = con_av.conInds{1}(2);
+    gi = con_av.headModel.gain(:, i * 2 - 1 : 2 * i);
+    gj = con_av.headModel.gain(:, j * 2 - 1 : 2 * j);
+    Gij = kron(gi,gj);
+    Gji = kron(gj,gi);
+    GG = [Gji, Gij];
+    [u,s,v] = svd(GG);
+    rnk_range =  1:8;
+    CT_proj_2 = CT_proj_1 - u(:,rnk_range) * (u(:,rnk_range)' * CT_proj_1);
 
-figure; con.PlotViews(0.1, 2);
+    [u_re,~,~] = msvd(real(CT_proj_2));
+    [CS_re_2, IND] = ps.PSIICOS_ScanFast(HM.gain, u_re(:,i_comp));
+
+    con_inds_re{i_comp} = ups.threshold_connections(CS_re_2, threshold, IND);
 
 
-% plot(v(1,:))
-% [corr, Cpvec, Upwr] = 
-colors = ups.plt.GetColors(3);
+    threshold = 30;
+    ch_path = [protocol_path, '/data/biomag2010/@rawdataset02/channel_ctf_acc1.mat'];
+    ch = load(ch_path);
 
-figure;
-c = zeros(4,3);
-for i = 1:4
-    c(i,:) = colors(i + 1).RGB;
-    plot(v(:,i), 'LineWidth', 2, 'Color', colors(i + 1).RGB);
-    hold on;
-end
+    i_band = 3;
+    freq_band = bands{i_band};
+    tr_filt= ltr(data_path, time_range, freq_band, HM);
+    CT = calc_CT(tr_filt, true);
 
-legend('comp1', 'comp2', 'comp3', 'comp4');
+    i_comp = 1;
+
+    rn = 100;
+    Upwr = ps.GetUpwrComplete(HM.gain, rn);
+    CT_proj = CT - Upwr * (Upwr' * CT);
+    [u_re,s_re,v_re] = svd(real(CT_proj));
+    % [u_re,s_re,v_re] = svd(real(CT));
+    [CS_re, IND] = ps.PSIICOS_ScanFast(HM.gain, u_re(:,i_comp));
+    con_inds_re{i_comp} = ups.threshold_connections(CS_re, threshold, IND);
+    con = ups.Bundles(con_inds_re{i_comp}, HM, CtxInfl);
+    figure('Name', num2str(rn));
+    con.Plot();
+
+    uu = u_re(:,i_comp);
+    uu = reshape(uu,33,33);
+    uu = HM.UP' * uu * HM.UP;
+    uu = abs(uu);
+    [i,j] = find(uu>0.02);
+    ups.plt.DrawConnectionsOnSensors([i,j], ch_path, true);
+
+%     con_m = con.Merge();
+%     cc = con_m.Clusterize(1, 0.0001);
+%     cca  = cc.Average();
+%     ccam = cca.Merge();
+%     ccaa = ccam.Average();
+%     cca.conInds = {ccaa.conInds{:}, cca.conInds{:}};
+
+%     n_clust = length(cc.conInds);
+%     sizes = zeros(n_clust,1);
+%     linewidth = zeros(n_clust,1);
+
+%     for i_clust = 1:length(cca.conInds)
+%         sizes(i_clust) = size(cc.conInds{i_clust},1);
+%     end
+
+
+
+%     linewidth =  floor(sizes / 30) + 1;
+%     linewidth = ones(size(sizes));
+%     linewidth_ = [12; linewidth]
+%     m_radii = 0.001 * (1 + 0.4 * log(sizes));
+%     m_radii = ones(size(sizes)) * 0.001;
+%     m_radii_ = [0.005; m_radii];
+%     icols = ones(size(m_radii_));
+%     icols(1) = 2;
+%     alpha = 0.2 * ones(size(m_radii_));
+%     alpha(1) = 1;
+%     cca.PlotViews(0.2, linewidth_, m_radii_, icols, alpha)
+%     % % con.CT = CT_proj;
+%     % con.CT = CT_proj;
+%     % con_av = con.Average();
+%     % con_av.PlotViews(0.2);
+
+
+%     % vv = [vs{:}];
+%     % m_v = mean(vv,2);
+%     % std_v = std(vv,[],2) / 10;
+%     % errorbar(m_v, std_v);
+%     % figure; con.PlotViews(0.1, 2);
+
+
+%     % plot(v(1,:))
+%     % [corr, Cpvec, Upwr] =
+%     % colors = ups.plt.GetColors(3);
+
+%     % figure;
+%     % c = zeros(4,3);
+%     % for i = 1:4
+%     %     c(i,:) = colors(i + 1).RGB;
+%     %     plot(v(:,i), 'LineWidth', 2, 'Color', colors(i + 1).RGB);
+%     %     hold on;
+%     % end
+
+%     % legend('comp1', 'comp2', 'comp3', 'comp4');
+%  1}}} %
