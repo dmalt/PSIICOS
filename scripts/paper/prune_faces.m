837899c1a29b15abf40228e5dab7d9e641728a7d
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-12-12 20:32:46 +0300

    remove folders with old scripts

--- scripts/paper/prune_faces.m
+++
@@ -1,7 +0,0 @@
-function faces_ = prune_faces(faces, idx)
-% Remove faces if one of 3 vertex indices is in idx
-% ________________________________________________
-
-	mask_faces = ismember(faces(:,1), idx) | ismember(faces(:,2), idx) |  ismember(faces(:,3), idx);
-	faces_ = faces(mask_faces,:);
-end
