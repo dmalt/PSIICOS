7317d3b587503b29e12fd6288ee0dafd675fa038
Dmitriy Altukhov <dm.altukhov@ya.ru>
2018-02-05 18:36:20 +0300

    handle the last block

--- scripts/simulations/PSIICOS_ScanFastGPU.m
+++ scripts/simulations/PSIICOS_ScanFastGPU.m
@@ -1,4 +1,4 @@
-function [Cs, IND] = PSIICOS_ScanFastGPU(G2dU, Cp, is_imag)
+function [Cs, IND] = PSIICOS_ScanFastGPU(G2dU, Cp, is_imag, blocksize)
 % -------------------------------------------------------------------------
 % Perform scanning algorithm to find strongest connections using correlation
 % of cross-spectrum with the forward operator
@@ -61,25 +61,23 @@ function [Cs, IND] = PSIICOS_ScanFastGPU(G2dU, Cp, is_imag)
     G2dU_gpu = gpuArray(single(G2dU));
     % G2dU_gpu = (single(G2dU));
     Cs = zeros(Nsrc, 'single', 'gpuArray');
-    blocksize = 1000;
-    blocksize_2 = blocksize / 2;
+    blocksize_x2 = blocksize * 2;
+    n_blocks = floor(Nsrc / blocksize); % number of equal blocks; remaining elements are treated separately
+    is_last_block = mod(Nsrc, blocksize);
+
 
 
     % p = 1;
     for iComp = 1:n_comp
-        for ii = 1:30
+        for ii = 1:n_blocks
             Cp_sq = gpuArray(single(reshape(Cp(:,iComp), Nsns, Nsns)));
             % Cp_sq =(single(reshape(Cp(:,iComp), Nsns, Nsns)));
             % ss = G2dU_gpu(:,1 + blocksize * (ii - 1): blocksize * ii)' * Cp_sq * G2dU_gpu;
-            % a11 = ss(1:2:end,1:2:end);
-            % a22 = ss(2:2:end,2:2:end);
-            % a12 = ss(1:2:end,2:2:end);
-            % a21 = ss(2:2:end,1:2:end);
 
-            a11 = G2dU_gpu(:,1 + blocksize * (ii - 1):2: blocksize * ii)' * Cp_sq * G2dU_gpu(:,1:2:end);
-            a12 = G2dU_gpu(:,1 + blocksize * (ii - 1):2: blocksize * ii)' * Cp_sq * G2dU_gpu(:,2:2:end);
-            a21 = G2dU_gpu(:,2 + blocksize * (ii - 1):2: blocksize * ii)' * Cp_sq * G2dU_gpu(:,1:2:end);
-            a22 = G2dU_gpu(:,2 + blocksize * (ii - 1):2: blocksize * ii)' * Cp_sq * G2dU_gpu(:,2:2:end);
+            a11 = G2dU_gpu(:,1 + blocksize_x2 * (ii - 1):2: blocksize_x2 * ii)' * Cp_sq * G2dU_gpu(:,1:2:end);
+            a12 = G2dU_gpu(:,1 + blocksize_x2 * (ii - 1):2: blocksize_x2 * ii)' * Cp_sq * G2dU_gpu(:,2:2:end);
+            a21 = G2dU_gpu(:,2 + blocksize_x2 * (ii - 1):2: blocksize_x2 * ii)' * Cp_sq * G2dU_gpu(:,1:2:end);
+            a22 = G2dU_gpu(:,2 + blocksize_x2 * (ii - 1):2: blocksize_x2 * ii)' * Cp_sq * G2dU_gpu(:,2:2:end);
 
             a11_c = conj(a11);
             a22_c = conj(a22);
@@ -104,7 +102,7 @@ function [Cs, IND] = PSIICOS_ScanFastGPU(G2dU, Cp, is_imag)
             clear a21_c;
 
             % Cs = abs(0.5 * (a11 + a22) + sqrt(0.25 * (a11 - a22) .^ 2 + a12 .* a21)) .^ 2;
-            Cs(1 + blocksize_2 * (ii - 1): blocksize_2 * ii, :) = (0.5 * (a11_2 + a12_2 + a21_2 + a22_2) + ...
+            Cs(1 + blocksize * (ii - 1): blocksize * ii, :) = (0.5 * (a11_2 + a12_2 + a21_2 + a22_2) + ...
             sqrt( 0.25 * (a11_2 + a12_2 - a21_2 - a22_2) .^ 2 +  a11_2 .* a21_2 + ll + a22_2 .* a12_2));
 
             clear a11;
@@ -130,6 +128,50 @@ function [Cs, IND] = PSIICOS_ScanFastGPU(G2dU, Cp, is_imag)
             % clear T
             % clear D
         end
+        % -------------------- Deal with the remaining elements -------------------- %
+        if is_last_block
+            Cp_sq = gpuArray(single(reshape(Cp(:,iComp), Nsns, Nsns)));
+            % Cp_sq =(single(reshape(Cp(:,iComp), Nsns, Nsns)));
+            % ss = G2dU_gpu(:,1 + blocksize * (ii - 1): blocksize * ii)' * Cp_sq * G2dU_gpu;
+
+            a11 = G2dU_gpu(:,1 + blocksize_x2 * n_blocks:2: end)' * Cp_sq * G2dU_gpu(:,1:2:end);
+            a12 = G2dU_gpu(:,1 + blocksize_x2 * n_blocks:2: end)' * Cp_sq * G2dU_gpu(:,2:2:end);
+            a21 = G2dU_gpu(:,2 + blocksize_x2 * n_blocks:2: end)' * Cp_sq * G2dU_gpu(:,1:2:end);
+            a22 = G2dU_gpu(:,2 + blocksize_x2 * n_blocks:2: end)' * Cp_sq * G2dU_gpu(:,2:2:end);
+
+            a11_c = conj(a11);
+            a22_c = conj(a22);
+            a12_c = conj(a12);
+            a21_c = conj(a21);
+            % clear ss;
+
+            a11_2 = real(a11 .* a11_c);
+            a22_2 = real(a22 .* a22_c);
+            a12_2 = real(a12 .* a12_c);
+            a21_2 = real(a21 .* a21_c);
+
+            ll = 2 * real(a11 .* a22 .* a12_c .* a21_c);
+
+            clear a11;
+            clear a12;
+            clear a22;
+            clear a21;
+            clear a11_c;
+            clear a22_c;
+            clear a12_c;
+            clear a21_c;
+
+            % Cs = abs(0.5 * (a11 + a22) + sqrt(0.25 * (a11 - a22) .^ 2 + a12 .* a21)) .^ 2;
+            Cs(1 + blocksize * n_blocks:end, :) = (0.5 * (a11_2 + a12_2 + a21_2 + a22_2) + ...
+            sqrt( 0.25 * (a11_2 + a12_2 - a21_2 - a22_2) .^ 2 +  a11_2 .* a21_2 + ll + a22_2 .* a12_2));
+
+            clear a11;
+            clear a22;
+            clear a12;
+            clear a21;
+        end
+        % -------------------------------------------------------------------------- %
+
         Cs = gather(Cs);
         Cs = nonzeros(triu(Cs,1)');
 
